6fe264fee9633807a540c9d1891c4585dc0b7232
HAYU9013 <114861748+HAYU9013@users.noreply.github.com>
2024-08-27 19:58:01 +0800

    feat: add sqlite support

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"backend/controllers"
 	"backend/middleware"
 	"backend/routes"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
+
+	// Initialize GORM with SQLite
+	db, err := gorm.Open(sqlite.Open("service.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database")
+	}
+	// Auto-migrate the User model
+	db.AutoMigrate(&controllers.User{})
+	// 設定資料庫實例到控制器
+	controllers.SetDB(db)
+
 	r := gin.Default()
 
 	// 設定全域的 CORS middleware
