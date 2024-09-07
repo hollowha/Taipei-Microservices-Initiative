7cb5345ec39750e08fa658e136d82eefaad0a1d6
Chen En <122762161+Ennn-1023@users.noreply.github.com>
2024-09-07 18:47:33 +0800

    gemini api key setting

--- backend/models/geminiInit.go
+++ backend/models/geminiInit.go
@@ -3,7 +3,6 @@ package models
 import (
 	"context"
 	"log"
-	"os"
 
 	"github.com/google/generative-ai-go/genai"
 	"google.golang.org/api/option"
@@ -12,13 +11,9 @@ import (
 var GeminiClient *genai.Client
 
 func InitGeminiClient() {
-	apiKey, ok := os.LookupEnv("API_KEY")
-	if !ok {
-		log.Fatalf("API_KEY not found")
-	}
 
 	ctx := context.Background()
-	client, err := genai.NewClient(ctx, option.WithAPIKey(apiKey))
+	client, err := genai.NewClient(ctx, option.WithAPIKey("AIzaSyAsrQ5ObzfFJfGjwAIaMcRxp4gW-PMiELg"))
 	if err != nil {
 		log.Fatalf("Error creating client: %v", err)
 	}
