9f10516b1572f2c20df53c0c1ba13b7762a3674e
余欣鴻 <76166970+hollowha@users.noreply.github.com>
2024-09-07 18:01:34 +0800

    Update exampleControlleer.go
    message *2->*1

--- backend/controllers/exampleControlleer.go
+++ backend/controllers/exampleControlleer.go
@@ -9,7 +9,7 @@ import (
 // Example 回應簡單的 JSON 資料
 func Example(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello, this is an example??!\nfrom the air",
+		
 		"message": "Hello, this is an example! XDDDDDDD",
 	})
 }
