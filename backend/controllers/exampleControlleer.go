bd3de6c40b8675b8fa89fc05b6a160f81cf156e5
Kevin171717 <kevin47338647@gmail.com>
2024-09-07 11:45:26 +0800

    add service.db

--- backend/controllers/exampleControlleer.go
+++ backend/controllers/exampleControlleer.go
@@ -10,6 +10,5 @@ import (
 func Example(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Hello, this is an example??!\nfrom the air",
-		"message": "Hello, this is an example! XDDDDDDD",
 	})
 }
